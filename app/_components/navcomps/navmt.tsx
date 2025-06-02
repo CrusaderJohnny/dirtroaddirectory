9f742d3b8e9415ce48b065937d1e44949f039bcf
Daniel A. <danielasefa975@gmail.com>
2025-06-02 15:57:32 -0600

    Deleted old header

--- app/_components/navcomps/navmt.tsx
+++ app/_components/navcomps/navmt.tsx
@@ -20,20 +20,22 @@ export default function NavMT() {
             <AppShell.Header>
                 {/* main div containing the header */}
                 <BackgroundImage src="https://letspasta.com/wp-content/uploads/2022/08/Alberta-farming.jpg"
-                                 style={{
-                                     backgroundColor: theme.colors.red[6], // Use if color preferred over image (team not decided)
-                                     height: '100%',
-                                     display: 'flex',
-                                     justifyContent: 'space-between',
-                                     alignItems: 'center',
-                                     padding: '0 3vh',
-                                 }}
+                    style={{
+                        backgroundColor: theme.colors.red[6], // Use if color preferred over image (team not decided)
+                        height: '100%',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        padding: '0 3vh',
+                    }}
                 >
                     {/* Header right side */}
                     <div
-                        style={{width:'25%',
+                        style={{
+                            width: '25%',
                             display: 'flex',
-                            justifyContent: 'space-between' }}
+                            justifyContent: 'space-between'
+                        }}
                     >
                         <Image
                             src='https://media.istockphoto.com/id/1170724138/vector/farmers-market-hand-drawn-lettering.jpg?s=1024x1024&w=is&k=20&c=EI--kDMvBM9pvC9jFJcaoepQHcDbTxp-De6fgIVqy_8='
