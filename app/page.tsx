94c8dd21c6e1f2dd884464773691221d7c705364
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-06-04 09:34:49 -0600

    added admin page and route protection with style changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,33 @@
 "use client"
-import { Button, Center, Container, Flex, Text } from "@mantine/core";
+import {AppShell, Button, Center, Container, Flex, Text} from "@mantine/core";
 import Link from "next/link";
 import Board from "@/app/_components/tiktaktoe/board";
+import NavMT from "@/app/_components/navcomps/navmt";
 
 export default function Home() {
   return (
-    <Container>
-      <Center>
+      <>
+      <NavMT/>
         <Container>
-          <Text p={20}>Hello World, What the....</Text>
-          <Flex direction="column" gap={20}>
-            <Button component={Link} href="market">Market Map</Button>
-            <Button component={Link} href='map'>Map</Button>
-            <Button component={Link} href='header'>Header</Button>
-            <Button component={Link} href='vendor'>Vendors</Button>
-            <Button component={Link} href='userpage'>User Profile</Button>
-          </Flex>
           <Center>
-            <Container mt={20}>
-              <Board/>
+            <Container>
+              <Text p={20}>Hello World, What the....</Text>
+              <Flex direction="column" gap={20}>
+                <Button component={Link} href="market">Market Map</Button>
+                <Button component={Link} href='map'>Map</Button>
+                <Button component={Link} href='header'>Header</Button>
+                <Button component={Link} href='vendor'>Vendors</Button>
+                <Button component={Link} href='userpage'>User Profile</Button>
+                <Button component={Link} href='admin'>Admin tools</Button>
+              </Flex>
+              <Center>
+                <Container mt={20}>
+                  <Board/>
+                </Container>
+              </Center>
             </Container>
           </Center>
         </Container>
-      </Center>
-    </Container>
+      </>
   );
 }
