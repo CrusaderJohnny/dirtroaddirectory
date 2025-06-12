3fc9c93e726f228bf9342b739b9f7b97c74a3f98
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-06-12 15:35:50 -0600

    Tidied up form submission files and main pages, ensured errors and warnings were handled or fixed to no longer cause an issue with type casting

--- app/admin-post/page.tsx
+++ app/admin-post/page.tsx
@@ -1,14 +1,18 @@
 import {AppShell, AppShellHeader, AppShellMain, Card, Center} from "@mantine/core";
 import NavMT from "@/app/_components/navcomps/navmt";
+import AdminPostForm from "@/app/_components/postmanagement/adminPostForm";
+import {currentUser} from "@clerk/nextjs/server";
 
 export default async function Page() {
-    // const user = await currentUser();
+    const user = await currentUser();
+    const serializedUser = user ? JSON.parse(JSON.stringify(user)) : null;
     return (
         <AppShell>
             <AppShellHeader component={NavMT}/>
             <AppShellMain>
-                <Center pt={20}>
+                <Center>
                     <Card shadow="lg" radius="md" withBorder w={"70rem"}>
+                        <AdminPostForm user={serializedUser}/>
                     </Card>
                 </Center>
             </AppShellMain>
