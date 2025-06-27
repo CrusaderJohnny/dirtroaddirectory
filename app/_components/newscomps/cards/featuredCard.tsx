9bcbb72ea160689afe2be3f40bdc3b6b12f661ec
Daniel A. <danielasefa975@gmail.com>
2025-06-26 19:55:08 -0600

    feature change

--- app/_components/newscomps/cards/featuredCard.tsx
+++ app/_components/newscomps/cards/featuredCard.tsx
@@ -8,10 +8,9 @@ export default function FeaturedCard( { article }: { article: ArticleInterface }
         <Link href={`/articleSubPage?articleId=${article.id}`} passHref>
             <Card withBorder radius="md" shadow='sm' p="md" w='100%' h='100%' bg='gray.1' className="cursor-pointer">
                 <Title order={3} >{article.title}</Title>
-                <Text size="sm" c="dimmed">{article.date}</Text>
 
-                {/*Type 1*/}
                 <Image src={article.imgLink} alt={article.title} h='60%' radius="md" />
+                <Text size="sm" c="dimmed">{article.date}</Text>
 
                 <Text p='sm'>{article.summary}</Text>
             </Card>
