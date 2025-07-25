e17ece16141f2824de5ed3b94f07753b844c782a
MacelroyDev <macehowald@gmail.com>
2025-07-24 18:19:17 -0600

    Revert "Merge branch 'main' into mace"
    This reverts commit a6835532c8886f40a2c8a689e84336cea2eb3bec, reversing
    changes made to 1f14c3f8381cb0a5331c31b080e8673215b6fdb9.

--- app/admin/edit-post/page.tsx
+++ app/admin/edit-post/page.tsx
@@ -1,8 +1,17 @@
 import {AppShell, AppShellFooter, AppShellHeader, AppShellMain, Button, Center} from "@mantine/core";
 import NavMT from "@/app/_components/navcomps/navmt";
 import ArticlePutForm from "@/app/_components/postmanagement/articlePutForm";
+import {currentUser} from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
+import {checkRole} from "@/_utils/roles";
 
 export default async function Page() {
+    const user = await currentUser();
+
+    if (!user || !await checkRole('admin')) {
+        redirect("/");
+    }
+
     return (
         <AppShell>
             <AppShellHeader component={NavMT}/>
