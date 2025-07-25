b929c8dbfafbdf6ce12d3aec495f504b81b161f6
MacelroyDev <macehowald@gmail.com>
2025-07-24 20:42:42 -0600

    removed redirect to match main

--- app/admin/add-post/page.tsx
+++ app/admin/add-post/page.tsx
@@ -2,19 +2,10 @@ import {AppShell, AppShellFooter, AppShellHeader, AppShellMain, Button, Center} 
 import NavMT from "@/app/_components/navcomps/navmt";
 import AdminPostForm from "@/app/_components/postmanagement/adminPostForm";
 import {currentUser} from "@clerk/nextjs/server";
-import {redirect} from "next/navigation";
-import {checkRole} from "@/_utils/roles";
+
 
 export default async function Page() {
     const user = await currentUser();
-
-    if(!user) {
-        redirect("/");
-    }
-
-    if(!await checkRole('admin')) {
-        redirect("/");
-    }
 
     const serializedUser = user ? JSON.parse(JSON.stringify(user)) : null;
 
